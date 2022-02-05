d4d8c78f7ae2b2fa33786988948d312a1165f84d
Achmad Sidik Fauzi <asidikfauzi@gmail.com>
2022-02-06 00:28:26 +0700

    modal pemain

--- resources/views/dashboard/admin/editPemain.blade.php
+++ resources/views/dashboard/admin/editPemain.blade.php
@@ -198,9 +198,29 @@
                 </div>
                 @endforeach
                 <div class="col-md-8 offset-md-4">
-                    <button type="submit" class="btn btn-primary">
-                        {{ __('SIMPAN') }}
-                    </button>
+                    <!-- Button trigger modal -->
+                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModalSave">
+                    Simpan
+                  </button>
+                  <!-- Modal -->
+                  <div class="modal fade" id="exampleModalSave" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Peringatan !</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Apakah anda yakin dengan perubahan ini ?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">Save Changes</button>
+                            
+                        </div>
+                        </div>
+                    </div>
+                  </div>
                 </div>
                 
             </div>
